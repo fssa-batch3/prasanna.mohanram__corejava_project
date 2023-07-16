87c1ae88dfb57249e3cc4f2e11a6e919479a079a
BrokenPRasanna <116252140+BrokenPRasanna@users.noreply.github.com>
2023-07-16 23:55:55 +0530

    Add files via upload

--- src/main/java/day07/practice/RemoveDuplicates.java
+++ src/main/java/day07/practice/RemoveDuplicates.java
@@ -1,21 +1,62 @@
 package day07.practice;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+//public class RemoveDuplicates {
+//	public static void main(String[] args) {
+//		List<Integer> numberList = new ArrayList<Integer>();
+//		numberList.add(5);
+//		numberList.add(4);
+//		numberList.add(3);
+//		numberList.add(2);
+//		numberList.add(3);
+//		numberList.add(1);
+//
+//		HashSet<Integer> uniqueNumbersSet = new HashSet<>(numberList);
+//
+//		for (Integer number : uniqueNumbersSet) {
+//			System.out.println(number);
+//		}
+//	}
+//}
 
 public class RemoveDuplicates {
-	public static void main(String[] args) {
-		List<Integer> numberList = new ArrayList<Integer>();
-		numberList.add(5);
-		numberList.add(4);
-		numberList.add(3);
-		numberList.add(2);
-		numberList.add(3);
-		numberList.add(1);
-
-		HashSet<Integer> uniqueNumbersSet = new HashSet<>(numberList);
-
-		for (Integer number : uniqueNumbersSet) {
-			System.out.println(number);
+	
+	public static boolean addnumtoarraylist(ArrayList<Integer> arrList) throws IllegalArgumentException {
+		
+		// if the arraylist are null it will throw the exception otherwise it will execute the below conditions
+		if(arrList == null) {
+			throw new IllegalArgumentException("ArrayList is Cann't be Null");
 		}
+		
+		arrList.add(0);
+		arrList.add(0);
+		arrList.add(1);
+		arrList.add(1);
+		arrList.add(2);
+		arrList.add(2);
+		arrList.add(5);
+		
+		System.out.println(arrList);
+		
+		// created the hashset Integer it will remove the duplicates
+		HashSet<Integer> setOfNum = new HashSet<Integer>(arrList);
+		
+		System.out.println(setOfNum);
+		
+		return true;
+		
 	}
+
+	
+	public static void main(String[] args) {
+		
+		// Created a empty Array List  to passing the array to the addnumtoarraylist method/
+		ArrayList<Integer> arrList = new ArrayList<>();
+		
+		addnumtoarraylist(arrList);
+		
+	}
+
 }
--- src/main/java/day07/practice/RemovingDuplicateTasks.java
+++ src/main/java/day07/practice/RemovingDuplicateTasks.java
@@ -3,67 +3,158 @@ package day07.practice;
 import java.time.*;
 import java.util.*;
 
-public class RemovingDuplicateTasks {
+//public class RemovingDuplicateTasks {
+//	private int id;
+//	private String name;
+//	private LocalDate deadline;
+//
+//	public RemovingDuplicateTasks(int id, String name, LocalDate deadline) {
+//		this.id = id;
+//		this.name = name;
+//		this.deadline = deadline;
+//	}
+//
+//	public int getId() {
+//		return id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public LocalDate getDeadline() {
+//		return deadline;
+//	}
+//
+//	public boolean equals(Object obj) {
+//		if (obj == this) {
+//			return true;
+//		}
+//		if (obj == null || obj.getClass() != this.getClass()) {
+//			return false;
+//		}
+//		RemovingDuplicateTasks otherTask =  (RemovingDuplicateTasks) obj;
+//		return id == otherTask.id && name.equals(otherTask.name) && deadline.equals(otherTask.deadline);
+//	}
+//
+//	public int hashCode() {
+//		return id ^ name.hashCode() ^ deadline.hashCode();
+//	}
+//
+//	public String toString() {
+//		return "Task ID: " + id + ", Name: " + name + ", Deadline: " + deadline;
+//	}
+//
+//	public static void main(String[] args) {
+//		ArrayList<RemovingDuplicateTasks> taskList = new ArrayList<>();
+//		taskList.add(new RemovingDuplicateTasks(1, "Task 1", LocalDate.parse("2023-07-15")));
+//		taskList.add(new RemovingDuplicateTasks(2, "Task 2", LocalDate.parse("2023-07-15")));
+//		taskList.add(new RemovingDuplicateTasks(3, "Task 3", LocalDate.parse("2023-07-16")));
+//		taskList.add(new RemovingDuplicateTasks(1, "Task 1", LocalDate.parse("2023-07-15")));
+//
+//		System.out.println("Original Task List:");
+//		for (RemovingDuplicateTasks task : taskList) {
+//			System.out.println(task);
+//		}
+//
+//		HashSet<RemovingDuplicateTasks> uniqueTasks = new HashSet<>(taskList);
+//		taskList.clear();
+//		taskList.addAll(uniqueTasks);
+//
+//		System.out.println("\nTask List after removing duplicates:");
+//		for (RemovingDuplicateTasks task : taskList) {
+//			System.out.println(task);
+//		}
+//	}
+//}
+
+
+
+
+
+class Task {
+
+	// Created Getter and Setter Methods for the Attributes
 	private int id;
 	private String name;
 	private LocalDate deadline;
 
-	public RemovingDuplicateTasks(int id, String name, LocalDate deadline) {
-		this.id = id;
-		this.name = name;
-		this.deadline = deadline;
-	}
-
 	public int getId() {
 		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public LocalDate getDeadline() {
 		return deadline;
 	}
 
-	public boolean equals(Object obj) {
-		if (obj == this) {
-			return true;
-		}
-		if (obj == null || obj.getClass() != this.getClass()) {
-			return false;
-		}
-		RemovingDuplicateTasks otherTask = (RemovingDuplicateTasks) obj;
-		return id == otherTask.id && name.equals(otherTask.name) && deadline.equals(otherTask.deadline);
+	public void setDeadline(LocalDate deadline) {
+		this.deadline = deadline;
 	}
 
-	public int hashCode() {
-		return id ^ name.hashCode() ^ deadline.hashCode();
+	public Task(int id, String name, LocalDate deadline) {
+
+		this.id = id;
+		this.name = name;
+		this.deadline = deadline;
 	}
 
-	public String toString() {
-		return "Task ID: " + id + ", Name: " + name + ", Deadline: " + deadline;
+}
+
+public class RemovingDuplicateTasks {
+
+	public static boolean removeduplicates(ArrayList<Task> tasks) throws IllegalArgumentException {
+		
+		if(tasks == null) {
+			throw new IllegalArgumentException("HashSet is connot be null");
+		}
+
+		HashSet<Task> hashOfSet = new HashSet<Task>(tasks);
+
+		System.out.println(hashOfSet);
+
+		return true;
+
+	}
+
+	public static boolean addTaskToArray(ArrayList<Task> taskList) throws IllegalArgumentException {
+
+		if (taskList == null) {
+			throw new IllegalArgumentException("ArrayList Is Cann't Be Null");
+		}
+
+		String day1 = "2023-07-14";
+		LocalDate date1 = LocalDate.parse(day1);
+		Task t1 = new Task(1, "Read Books", date1);
+
+		taskList.add(t1);
+		taskList.add(new Task(2, "Read Notes", date1));
+		taskList.add(t1);
+		taskList.add(new Task(3, "Learn Java", date1));
+
+		removeduplicates(taskList);
+
+		return true;
+
 	}
 
 	public static void main(String[] args) {
-		ArrayList<RemovingDuplicateTasks> taskList = new ArrayList<>();
-		taskList.add(new RemovingDuplicateTasks(1, "Task 1", LocalDate.parse("2023-07-15")));
-		taskList.add(new RemovingDuplicateTasks(2, "Task 2", LocalDate.parse("2023-07-15")));
-		taskList.add(new RemovingDuplicateTasks(3, "Task 3", LocalDate.parse("2023-07-16")));
-		taskList.add(new RemovingDuplicateTasks(1, "Task 1", LocalDate.parse("2023-07-15")));
 
-		System.out.println("Original Task List:");
-		for (RemovingDuplicateTasks task : taskList) {
-			System.out.println(task);
-		}
+		ArrayList<Task> taskList = new ArrayList<>();
+		
+		addTaskToArray(taskList);
 
-		HashSet<RemovingDuplicateTasks> uniqueTasks = new HashSet<>(taskList);
-		taskList.clear();
-		taskList.addAll(uniqueTasks);
-
-		System.out.println("\nTask List after removing duplicates:");
-		for (RemovingDuplicateTasks task : taskList) {
-			System.out.println(task);
-		}
 	}
+
 }

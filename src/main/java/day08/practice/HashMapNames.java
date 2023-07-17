e59ec059e66ce6345fb49bdc1fd7429c85e0a899
BrokenPRasanna <116252140+BrokenPRasanna@users.noreply.github.com>
2023-07-17 21:47:22 +0530

    Add files via upload

--- src/main/java/day08/practice/HashMapNames.java
+++ src/main/java/day08/practice/HashMapNames.java
@@ -2,25 +2,47 @@ package day08.practice;
 
 import java.util.*;
 
+//public class HashMapNames {
+//	public static void main(String[] args) {
+//
+//		String[] s = { "Ram", "Ram", "Superman", "Spider", "hey", "hello", "hey", "Spider" };
+//		Map<String, Integer> nameMap = new TreeMap<String, Integer>();
+//
+//		for (int i = 0; i < s.length; i++) {
+//			if (nameMap.get(s[i]) == null) {
+//				nameMap.put(s[i], 1);
+//			} else {
+//				int count = nameMap.get(s[i]);
+//				count++;
+//				nameMap.put(s[i], count);
+//			}
+//		}
+//		for (String names : nameMap.keySet()) {
+//
+//			Integer count = nameMap.get(names);
+//			System.out.println(names + ": " + count);
+//		}
+//	}
+//}
+
 public class HashMapNames {
 	public static void main(String[] args) {
-
 		String[] s = { "Ram", "Ram", "Superman", "Spider", "hey", "hello", "hey", "Spider" };
-		Map<String, Integer> nameMap = new TreeMap<String, Integer>();
+		Map<String, Integer> nameMap = countNames(s);
 
-		for (int i = 0; i < s.length; i++) {
-			if (nameMap.get(s[i]) == null) {
-				nameMap.put(s[i], 1);
-			} else {
-				int count = nameMap.get(s[i]);
-				count++;
-				nameMap.put(s[i], count);
-			}
-		}
 		for (String names : nameMap.keySet()) {
-
 			Integer count = nameMap.get(names);
 			System.out.println(names + ": " + count);
 		}
+	}
+
+	public static Map<String, Integer> countNames(String[] namesArray) {
+		Map<String, Integer> nameMap = new TreeMap<>();
+
+		for (String name : namesArray) {
+			nameMap.put(name, nameMap.getOrDefault(name, 0) + 1);
+		}
+
+		return nameMap;
 	}
 }
--- src/main/java/day08/practice/ListOfEmployees.java
+++ src/main/java/day08/practice/ListOfEmployees.java
@@ -1,42 +1,74 @@
 package day08.practice;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
+//public class ListOfEmployees {
+//	public static void main(String[] args) {
+//
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("How many persons you want to give?");
+//		int n = sc.nextInt();
+//
+//		Map<String, List<String>> departMap = new HashMap<>();
+//
+//		for (int i = 0; i < n; i++) {
+//			System.out.println("Enter Input Like: HR, Name");
+//			String s = sc.next();
+//
+//			if (s == null || "".equals(s)) {
+//				throw new IllegalArgumentException("Input cann't be Empty or Null");
+//			}
+//
+//			String[] departName = s.split(",");
+//
+//			if (departName.length < 2 || departName.length > 2) {
+//				throw new IllegalArgumentException("Input Must hava Department and the Employee Name");
+//			}
+////			System.out.println(departName);
+//
+//			if (departMap.get(departName[0]) == null) {
+//				departMap.put(departName[0], new ArrayList<>());
+//				departMap.get(departName[0]).add(departName[1]);
+//			} else {
+//				departMap.get(departName[0]).add(departName[1]);
+//			}
+//		}
+//		System.out.println(departMap);
+//	}
+//}
 
 public class ListOfEmployees {
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 		System.out.println("How many persons you want to give?");
 		int n = sc.nextInt();
 
 		Map<String, List<String>> departMap = new HashMap<>();
 
-		for (int i = 0; i < n; i++) {
+		int i = 0;
+		while (i < n) {
 			System.out.println("Enter Input Like: HR, Name");
 			String s = sc.next();
 
 			if (s == null || "".equals(s)) {
-				throw new IllegalArgumentException("Input cann't be Empty or Null");
+				throw new IllegalArgumentException("Input cannot be Empty or Null");
 			}
 
 			String[] departName = s.split(",");
 
-			if (departName.length < 2 || departName.length > 2) {
-				throw new IllegalArgumentException("Input Must hava Department and the Employee Name");
+			if (departName.length != 2) {
+				throw new IllegalArgumentException("Input Must have Department and the Employee Name");
 			}
-//			System.out.println(departName);
 
-			if (departMap.get(departName[0]) == null) {
+			if (!departMap.containsKey(departName[0])) {
 				departMap.put(departName[0], new ArrayList<>());
-				departMap.get(departName[0]).add(departName[1]);
-			} else {
-				departMap.get(departName[0]).add(departName[1]);
 			}
+
+			departMap.get(departName[0]).add(departName[1]);
+
+			i++;
 		}
+
 		System.out.println(departMap);
 	}
 }

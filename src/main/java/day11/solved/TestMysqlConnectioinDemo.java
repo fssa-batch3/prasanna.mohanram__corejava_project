26c1aca9452d631ab0ff9cc8fad7ed9ec53659fa
BrokenPRasanna <116252140+BrokenPRasanna@users.noreply.github.com>
2023-07-24 10:53:07 +0530

    Add files via upload

---
+++ src/main/java/day11/solved/TestMysqlConnectioinDemo.java
@@ -0,0 +1,19 @@
+package day11.solved;
+
+import java.sql.*;
+
+
+public class TestMysqlConnectioinDemo {
+
+	public static void main(String[] args) throws SQLException {
+
+		String url = "jdbc:mysql://localhost:3306/fssa";
+		String userName = "root";
+		String password = "123456";
+		
+		Connection con = DriverManager.getConnection(url, userName, password);
+		
+		System.out.println("connection created...");
+	       con.close();
+	}
+}
